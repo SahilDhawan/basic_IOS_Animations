85c2c74bf7d588e634366144a3a3eaafae55ce57
Sahil Dhawan <sahildhawan2012@icloud.com>
2016-10-24 00:56:14 +0530

    Initial Commit

---
+++ basicanimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  basicanimations
+//
+//  Created by Sahil Dhawan on 24/10/16.
+//  Copyright © 2016 Sahil Dhawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
