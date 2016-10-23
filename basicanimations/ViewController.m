81ed4d0a7198ecd2aeaf71ef02080c2997fc9561
Sahil Dhawan <sahildhawan2012@icloud.com>
2016-10-24 01:58:51 +0530

    second commit

--- basicanimations/ViewController.m
+++ basicanimations/ViewController.m
@@ -9,11 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
-
+int i=0;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +25,29 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)fadeButtonPressed:(id)sender {
+    [UIView animateWithDuration:3 animations:^{
+        self.imageView.alpha = 0;
+    } completion:^(BOOL finished) {
+        self.imageView.alpha = 1;
+    }];
+}
+- (IBAction)slideHPressed:(id)sender {
+    [UIView animateWithDuration:3 animations:^{
+        
+        self.imageView.center = CGPointMake(self.imageView.center.x + self.view.frame.size.width,self.imageView.center.y);
+    } completion:^(BOOL finished) {
+        self.imageView.center = CGPointMake(self.view.frame.size.width/2,self.imageView.center.y);
+    }];
+}
 
+- (IBAction)slideVPressed:(id)sender {
+    [UIView animateWithDuration:3 animations:^{
+        
+        self.imageView.center = CGPointMake(self.view.frame.size.width/2,self.view.frame.size.height + self.imageView.frame.size.height);
+    } completion:^(BOOL finished) {
+        self.imageView.center = CGPointMake(self.view.frame.size.width/2,self.view.frame.size.height/2);
+    }];
+}
 
 @end
